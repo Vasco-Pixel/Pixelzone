88c996d203dca9c29ac951f8255168a124b53aa6
Vasco-Pixel <41165853+Vasco-Pixel@users.noreply.github.com>
2019-07-29 14:27:36 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -7,8 +7,8 @@
 // @match        https://pixelzone.io/*
 // @match        http://pixelzone.io/*
 // @homepage     https://github.com/Vasco-Pixel/mz
-// @updateURL    https://raw.githubusercontent.com/Vasco-Pixel/mz/master/minimap.user.js
-// @downloadURL  https://raw.githubusercontent.com/Vasco-Pixel/mz/master/minimap.user.js
+// @updateURL    https://raw.githubusercontent.com/Vasco-Pixel/Pixelzone/master/minimap.user.js
+// @downloadURL  https://raw.githubusercontent.com/Vasco-Pixel/Pixelzone/master/minimap.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Vasco-Pixel/mz/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Vasco-Pixel/Pixelzone/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
